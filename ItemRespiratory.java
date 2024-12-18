4d5873d21df228846ef5770b87d2a07c3a664309
Samarth292005 <samarthagarwal6277@gmail.com>
2024-12-18 15:19:33 +0530

    Create ItemRespiratory.java

---
+++ ItemRespiratory.java
@@ -0,0 +1,8 @@
+package com.example.godown;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByCategory(String category);
+}
